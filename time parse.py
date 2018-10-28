9a876a22708435871b9c4e93b4cc166bee928210
Adam Cohan <adamcohan@Adams-MacBook-Pro-2.local>
2018-10-28 10:28:15 -0700

    worked on fixing why % error was always off (check text block for details), but still off by +0.08%

--- time parse.py
+++ time parse.py
@@ -5,9 +5,13 @@ import random
 
 '''
 TO DO
-* figure out why meanpercenterror is always off (-1.58%)
+* figure out why meanpercenterror is always off
 * mess around and figure out right numbers for samplesize and numsamples
-* round the mean
+
+changes that affected % error
+* took out int() when adding times.m200[x] to sampleacc (good; more than just one decimal point)
+* rounded each mean that went into meanlist (good; no weird 19.679999999998 bs)
+* rounded mean and meanlistavg
 '''
 
 #testlist = [0,1,2,3,4,5,6,7,8,9]
@@ -27,21 +31,21 @@ for i in range(len(times.m200)):
 acc = 0
 for x in range(len(times.m200)):
     acc += times.m200[x]
-mean = acc / (len(times.m200) + 1)
+mean = (acc / (len(times.m200) + 1))
 
 meanlist = [] #where all the averages are appended to
 sampleacc = 0 #accumulates all the random times
 meanval = 0 #a mean time that gets added to meanlist
-samplesize = 1000 #size of a sample (n)
+samplesize = 20 #size of a sample (n)
 numsamples = 1000 #number of samples
 
 #do this bit (working out sample size and number of samples for mean distribution)
 #append each one to meanlist
 for i in range(numsamples):
     for i in range(samplesize):
-        x = random.randint(0,len(times.m200)-1) #I THINK THAT THIS IS WHERE THE PROBLEM IS
-        sampleacc += int(times.m200[x])
-    meanval = sampleacc / samplesize
+        x = random.randint(0,len(times.m200) - 1)
+        sampleacc += times.m200[x]
+    meanval = round((sampleacc / samplesize), 2)
     meanlist.append(meanval)
     sampleacc = 0
 
@@ -49,13 +53,13 @@ for i in range(numsamples):
 meanlistacc = 0
 for i in range(len(meanlist)):
     meanlistacc += meanlist[i]
-meanlistavg = meanlistacc / (len(meanlist) + 1)
+meanlistavg = (meanlistacc / (len(meanlist) + 1))
 
 #the % error of the mean of the sample mean distribution compared to the actual mean
 meanpercenterror = (meanlistavg - mean) / mean
 
 #all the garbage I print
-print(meanlist) #the list that will have the items for the sample mean distribution (normal)
-print('actual mean: ' + str(mean)) #the mean --> round to 2 decimals
-print('sample mean: ' + str(meanlistavg)) #the mean of the sample mean distribution
+#print(meanlist) #the list that will have the items for the sample mean distribution (normal)
+print('actual mean: ' + str(round(mean, 5))) #the mean
+print('sample mean: ' + str(round(meanlistavg, 5))) #the mean of the sample mean distribution
 print('percent error: ' + str(meanpercenterror * 100) + '%') #the percent error of the mean
